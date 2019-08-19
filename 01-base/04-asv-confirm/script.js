95f35228e56569b9c9038aa8de81eacd66bad563
RobinMarien <dev.robinmarien@gmail.com>
2019-08-19 15:27:10 +0200

    finished 04

--- 01-base/04-asv-confirm/script.js
+++ 01-base/04-asv-confirm/script.js
@@ -9,8 +9,17 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function x() {
 
     // your code here
+    var age_gender_town=prompt("Enter your age, your gender and your town (use , as a separator)","18,male, Antwerp"),
+        age=age_gender_town.split(",")[0],
+        gender=age_gender_town.split(",")[1],
+        town=age_gender_town.split(",")[2];
+
+    var answer = confirm("Your age is " + age +", your gender is " + gender + " and your town is " + town);
+    if (answer === false){
+        x();
+    }
 
 })();
