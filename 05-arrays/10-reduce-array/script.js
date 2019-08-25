6148cd69e702c225a2ab30cf4dcf0c677740ac2d
RobinMarien <dev.robinmarien@gmail.com>
2019-08-25 20:13:49 +0200

    finished 05-10

--- 05-arrays/10-reduce-array/script.js
+++ 05-arrays/10-reduce-array/script.js
@@ -90,5 +90,16 @@
     ];
 
     // your code here
+    document.getElementById("run").addEventListener("click", function(){
+        //The reduce () method reduces the array to a single value. The reduce() method executes a provided function for each value of the array (from left-to-right). The return value of the function is stored in an accumulator (result/total).
+
+        var sum = people.reduce((result, {age}) => {
+            return result + age;
+        }, 0);
+
+        console.log(sum)
+
+    })
+
 
 })();
