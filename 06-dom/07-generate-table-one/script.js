aadfe49c2b44428b201db48291131437160ad189
RobinMarien <dev.robinmarien@gmail.com>
2019-08-26 12:19:40 +0200

    finished 06-07

--- 06-dom/07-generate-table-one/script.js
+++ 06-dom/07-generate-table-one/script.js
@@ -13,4 +13,29 @@
 
     // your code here
 
+    //create table
+    var table = document.createElement("table");
+    table.style.width = '50%';
+    table.setAttribute('border', '1');
+
+    //put table in target element
+    document.getElementById("target").appendChild(table);
+
+    //create 10 table rows & 10 table data
+
+    for (var i = 0; i<10 ; i++) {
+        var row = document.createElement("tr");
+        var data = document.createElement("td");
+
+        //put row(s) in table
+        table.appendChild(row);
+        //put data in row
+        row.appendChild(data);
+
+        //put numbers inside the table
+        data.innerHTML = i + 1;
+    }
+
+
+
 })();
