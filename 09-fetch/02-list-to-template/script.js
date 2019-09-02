c904f84a9b8402d8d938a4ed34b01188d77e28af
RobinMarien <dev.robinmarien@gmail.com>
2019-09-02 14:31:34 +0200

    finished 09-02

--- 09-fetch/02-list-to-template/script.js
+++ 09-fetch/02-list-to-template/script.js
@@ -10,5 +10,25 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    // your code here       |       (https://dev.to/therealdanvega/html-template-tag-in-vanilla-javascript-and-vue-5eoo)
+
+    document.getElementById("run").addEventListener("click", function(){
+
+        fetch("http://localhost:63342/js-complete-course/_shared/api.json")
+            .then(function(response){
+                return response.json();
+            })
+            .then(function(data){
+                console.log(data.heroes);
+                data.heroes.forEach(function(hero){
+                    var temp = document.getElementById("tpl-hero").content.cloneNode(true);
+                    temp.querySelector(".name").innerHTML = hero.name;
+                    temp.querySelector(".alter-ego").innerHTML = hero.alterEgo;
+                    temp.querySelector(".powers").innerHTML = hero.abilities;
+
+                    document.getElementById("target").appendChild(temp);
+
+                })
+            })
+    })
 })();
