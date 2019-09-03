76513552ab6efbbc128deb695b0ff5c521c8779b
RobinMarien <dev.robinmarien@gmail.com>
2019-09-03 12:31:14 +0200

    finished 08-02 (with the class)

--- 08-asynchronous/02-get-comments/script.js
+++ 08-asynchronous/02-get-comments/script.js
@@ -11,4 +11,29 @@
 
 (() => {
     // your code here
+
+    let posts,comment;
+
+    document.getElementById("run").addEventListener("click", function(){
+
+        window.lib.getPosts(function(error,articles){
+
+            posts = articles;
+            posts.forEach(function(post){
+
+                window.lib.getComments(null,function(error, getComments){
+
+                    comment = getComments[post.id];
+
+                    if (comment !== undefined){
+                        post.comment = comment;
+                    }
+
+                    console.log(post);
+                })
+
+            })
+
+        })
+    })
 })();
