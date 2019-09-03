479664185dbcd5652af40dd062f5ac3a6122f0bf
RobinMarien <dev.robinmarien@gmail.com>
2019-09-03 09:50:10 +0200

    finished 09-04

--- 09-fetch/04-add/script.js
+++ 09-fetch/04-add/script.js
@@ -11,4 +11,29 @@
 
 (() => {
     // your code here
+
+
+        fetch("http://localhost:63342/js-complete-course/_shared/api.json")
+            .then(function(response){
+                return response.json();
+            })
+            .then(function(data){
+                //console.log(data.heroes);
+                var arr = data.heroes;
+
+                document.getElementById("run").addEventListener("click", function(){
+
+
+                var idNum = arr.length + 1;
+                var name = document.getElementById("hero-name").value;
+                var alter = document.getElementById("hero-alter-ego").value;
+                var power = document.getElementById("hero-powers").value;
+                power = power.split(", ");
+
+                arr.push({"id": idNum, "name": name, "alterEgo": alter, "abilities": power})
+
+                console.log(data.heroes);
+
+            })
+    })
 })();
